413290d8f3958cf4bf2307877f79199195a4029b
Sergey Muravyev <smurav@mail.ru>
2019-09-27 16:51:19 +0300

    Сохранение результатов обработки fastp в отдельный каталог

--- scripts/fastp.sh
+++ scripts/fastp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
+CONTAINER=fastp
+
 if [ -z "$1" ]; then
     echo "Укажите идентификатор образца"
     exit -1
 fi
-
-CONTAINER=fastp
 
 START=$(date +%s)
 BASENAME=$(basename -- "$0")
@@ -12,22 +13,33 @@ FILENAME="${BASENAME%.*}"
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 SAMPLEPATH=$(dirname $SCRIPTPATH)/samples/$1
-PREFIX=$SAMPLEPATH/$1
+PREFIX=$SAMPLEPATH/$1/$CONTAINER
 OUTFILE=$PREFIX.$CONTAINER.stdout
 ERRFILE=$PREFIX.$CONTAINER.stderr
 LOGFILE=$PREFIX.$CONTAINER.log
+
+# Каталог для сохранения результатов
+if [ -d $PREFIX ]; then
+	rm -rfv $PREFIX/*
+else
+	mkdir $PREFIX
+fi
+
 echo 'Контроль качества и обработка образца '$1 > $LOGFILE
 
 if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER})" ]; then
-	docker rm $CONTAINER
+	docker rm $CONTAINER | tee -a $LOGFILE
 fi
 
-CMD="docker run -v ${SAMPLEPATH}:/data --name ${CONTAINER} pgcbioinfo/fastp 
-fastp -i /data/${1}_R1_001.fastq.gz -I /data/${1}_R2_001.fastq.gz 
--o /data/${1}_R1_001.fp.fq.gz -O /data/${1}_R2_001.fp.fq.gz 
---json=${1}.fastp.json --html=${1}.fastp.html
---report_title='Отчёт fastp по образцу ${1}'
---adapter_fasta=/data/${1}.adapters.fa -f 6 -t 4 -c -g -x -p"
+CMD="docker run -v ${SAMPLEPATH}:/data --name ${CONTAINER} 
+pgcbioinfo/fastp fastp 
+-i /data/${1}_R1_001.fastq.gz 
+-I /data/${1}_R2_001.fastq.gz 
+-o /data/${1}_R1_001.${CONTAINER}.fastq.gz 
+-O /data/${1}_R2_001.${CONTAINER}.fastq.gz 
+-j /data/${CONTAINER}/${1}.${CONTAINER}.json 
+-h /data/${CONTAINER}/${1}.${CONTAINER}.html
+--adapter_fasta=/data/${CONTAINER}/${1}.adapters.fa -f 6 -t 4 -w 7 -c"
 echo $CMD>>$LOGFILE
 $CMD 1>$OUTFILE 2>$ERRFILE
 
